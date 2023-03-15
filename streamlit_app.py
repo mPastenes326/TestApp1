ab831d75b7b1b0b71c80d4718f7fad8d72eb1c9b
Ayomitunde Fafore <ayo@streamlit.io>
2023-03-15 09:46:36 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+def generate_table(rows, columns):
+    table = np.random.randn(rows, columns)
+    return table
+
+
+def create_dataframe(rows, columns):
+    df = pd.DataFrame(
+        generate_table(rows, columns),
+        columns=('col %d' % i for i in range(columns)))
+    return df
+
+
+st.title("Ayo's First Application :sunglasses:")
+st.header("Dashboard")
+
+data = create_dataframe(20, 5)
+st.subheader("Data Table")
+st.dataframe(data)
+st.subheader("Data Line Chart")
+st.line_chart(data)
+st.subheader("Data Bar Chart")
+st.bar_chart(data)
